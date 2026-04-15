3c16081d35fc3e8368dc29cba576ded81105c8b8
Nyveruus <106652476+Nyveruus@users.noreply.github.com>
2026-04-15 18:17:24 +0200

    rsync backup script

---
+++ backup/rsync-backup/backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+backup_src="/home/"
+backup_mount="/mnt/backup"
+backup_dest="$backup_mount/current"
+current_date=$(date +%Y-%m-%d)
+log_path="$backup_mount/logs"
+previous_files="$backup_mount/files_previous/$current_date"
+
+mkdir -p "$log_path"
+mkdir -p "$previous_files"
+
+rsync -av --delete --backup \
+  --backup-dir="$previous_files" "$backup_src" "$backup_dest" \
+  > "$log_path/backup_$current_date.log" \
+  2> "$log_path/backup_${current_date}_error.log"
